5d24f709f4fc42ad5e5a606a7b41abe2fc1a3b59
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-08-28 23:20:33 +0300

    Add icon to UserMenu

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/auth/operations';
 import { useAuth } from 'hooks';
+import { FcBusinessman } from 'react-icons/fc';
 import css from './UserMenu.module.css';
 
 export const UserMenu = () => {
@@ -9,6 +10,7 @@ export const UserMenu = () => {
 
   return (
     <div className={css.wrapper}>
+      <FcBusinessman size={'1.5em'} />
       <p className={css.username}>Welcome, {user.name}</p>
       <button type="button" onClick={() => dispatch(logOut())}>
         Logout
